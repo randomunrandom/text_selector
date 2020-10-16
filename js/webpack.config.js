89f14bc132ade551a1aff2797bdf9c271e14395d
Random <unrndm@gmail.com>
2020-10-16 21:48:41 +0300

    fix: fixed diff with template and bumped all possible versions to the same one

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -5,7 +5,7 @@ var version = require('./package.json').version;
 // stored in a separate local variable.
 var rules = [
     { test: /\.css$/, use: ['style-loader', 'css-loader']},
-    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    // { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
 ]
 
 
